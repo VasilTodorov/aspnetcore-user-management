97c273921946d7cc1a165ac8259043cc07e73544
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2025-11-23 15:28:40 +0200

    Initial commit

---
+++ AspnetcoreUserManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspnetcoreUserManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# aspnetcore-user-management
